6a0fa8ef501ff7b6c68221f67a413e3c79e7b7d7
KornelZ <kornelzaba@gmail.com>
2016-11-07 10:08:34 +0100

    views dla buy offer

---
+++ LGSA/LGSA/View/BuyOfferView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using LGSA.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LGSA.View
+{
+    /// <summary>
+    /// Interaction logic for BuyOfferView.xaml
+    /// </summary>
+    public partial class BuyOfferView : UserControl
+    { 
+        public BuyOfferView()
+        {
+            InitializeComponent();
+        }
+
+        private async void AddButtonClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new AddOfferDialog();
+            dialog.DataContext = this.DataContext;
+            bool? addOffer = dialog.ShowDialog();
+
+            if(addOffer == true)
+            {
+                await (this.DataContext as BuyOfferViewModel).AddOffer();
+            }
+        }
+    }
+}
